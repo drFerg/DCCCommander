c58738c2941c227ebf306c7795112059b7556e8a
Fergus <fergus.leahy@gmail.com>
2015-05-12 10:24:00 +0100

    Updated minimum example to new api

--- examples/CmdrArduino_minimum/CmdrArduino_minimum.ino
+++ examples/CmdrArduino_minimum/CmdrArduino_minimum.ino
@@ -8,11 +8,11 @@
 ********************/
 
 #include <DCCPacket.h>
-#include <DCCPacketQueue.h>
-#include <DCCPacketScheduler.h>
+#include <DCCCommandStation.h>
 
+#define BUTTON 4
 
-DCCPacketScheduler dps;
+DCCCommandStation dcc;
 unsigned int analog_value;
 char speed_byte, old_speed = 0;
 byte count = 0;
@@ -21,43 +21,34 @@ byte F0 = 0;
 
 void setup() {
   Serial.begin(115200);
-  dps.setup();
-
-  //set up button on pin 4
-  pinMode(4, INPUT);
-  digitalWrite(4, HIGH); //activate built-in pull-up resistor  
+  dcc.setup();
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH); //activate built-in pull-up resistor  
 }
 
 void loop() {
-  //handle reading button, controls F0
-  byte button_state = digitalRead(4); //high == not pushed; low == pushed
-  if(button_state && (button_state != prev_state))
-  {
-    //toggle!
-    F0 ^= 1;
-    Serial.println(F0,BIN);
-    dps.setFunctions0to4(3,DCC_SHORT_ADDRESS,F0);
+  /* handle reading button, controls F0 */
+  byte button_state = digitalRead(BUTTON); /* high == not pushed; low == pushed */
+  if (button_state && (button_state != prev_state)){
+    F0 ^= 1; /* toggle */
+    Serial.println(F0, BIN);
+    dcc.setFunctions0to4(3, DCC_ADDR_SHORT, F0);
   }
   prev_state = button_state;
 
-  //handle reading throttle
+  /* handle reading throttle */
   analog_value = analogRead(0);
-  speed_byte = (analog_value >> 2)-127 ; //divide by four to take a 0-1023 range number and make it 1-126 range.
-  if(speed_byte != old_speed)
-  {
-    if(speed_byte == 0) //this would be treated as an e-stop!
-    {
-      if(old_speed > 0) speed_byte = 1;
-      else speed_byte = -1;
+  speed_byte = (analog_value >> 2) - 127 ; //divide by four to take a 0-1023 range number and make it 1-126 range.
+  if(speed_byte != old_speed) {
+    if(speed_byte == 0) { /* would be treated as e-stop */
+      speed_byte = 1;
     }
     Serial.print("analog = ");
     Serial.println(analog_value, DEC);
     Serial.print("digital = ");
     Serial.println(speed_byte, DEC);
-    dps.setSpeed128(3,DCC_SHORT_ADDRESS,speed_byte);
+    dcc.setSpeed128(3, DCC_ADDR_SHORT, speed_byte, DCC_FORWARD);
     old_speed = speed_byte;
   }
-  dps.update();
-  
   ++count;
 }
