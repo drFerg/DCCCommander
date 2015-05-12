35d26c57b3a7665216782483b062ca43db8cbe5c
Fergus <fergus.leahy@gmail.com>
2015-05-12 09:55:15 +0100

    Updated cprint to work with different boards  - Used macro defines for serial ports  - Now compiles for Yun/Leonardo boards but needs testing

--- cprint.c
+++ cprint.c
@@ -2,27 +2,40 @@
 #include <Arduino.h>
 #include <stdio.h>
 
-int USART0SendByte(char u8Data, FILE *stream) {
-  if(u8Data == '\n') USART0SendByte('\r', 0);
-  //wait while previous byte is completed
-  while(!(UCSR0A&(1<<UDRE0))){};
-  // Transmit data
-  UDR0 = u8Data;
+/* Uno, Duemillenove, Mega etc. */
+#if defined(__AVR_ATmega168__) ||defined(__AVR_ATmega168P__) || defined(__AVR_ATmega328P__) \
+ || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  #define UCSRXA UCSR0A
+  #define UDREX  UDRE0
+  #define UDRX   UDR0
+  #define RXCX   RXC0
+#elif defined(__AVR_ATmega32U4__) /* Leonardo, Esplora, Yun etc. */
+  #define UCSRXA UCSR1A
+  #define UDREX  UDRE1
+  #define UDRX   UDR1
+  #define RXCX   RXC1
+#endif
+
+int USARTSendByte(char byte, FILE *stream) {
+  if(byte == '\n') USARTSendByte('\r', 0);
+  /* Wait while previous byte is completed */
+  while(!(UCSRXA & (1 << UDREX))){};
+  UDRX = byte; /* Send byte */
   return 0;
 }
-int USART0ReceiveByte(FILE *stream) {
-  uint8_t u8Data;
-  // Wait for byte to be received
-  while(!(UCSR0A&(1<<RXC0))){};
-  u8Data=UDR0;
-  //echo input data
-  USART0SendByte(u8Data,stream);
-  // Return received data
-  return u8Data;
+
+int USARTReceiveByte(FILE *stream) {
+  uint8_t byte;
+  /* Wait until a byte is received */
+  while (!(UCSRXA & (1 << RXCX))){};
+  byte = UDRX; /* Copy byte */
+  USARTSendByte(byte,stream); /* Echo byte */
+  return byte;
 }
-//set stream pointer
-FILE usart0_str = FDEV_SETUP_STREAM(USART0SendByte, USART0ReceiveByte, _FDEV_SETUP_RW);
+
+/* set stream pointer */
+FILE usart_str = FDEV_SETUP_STREAM(USARTSendByte, USARTReceiveByte, _FDEV_SETUP_RW);
 
 void cprint_init() {
-  stdin = stdout = &usart0_str;
+  stdin = stdout = &usart_str;
 }
