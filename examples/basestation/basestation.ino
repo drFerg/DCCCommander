a36ccb41826b4822aec4a826a7e7377c18f2e06f
Fergus <fergus.leahy@gmail.com>
2015-09-28 11:33:53 +0100

    Updated basestation sketch with improved point control

--- examples/basestation/basestation.ino
+++ examples/basestation/basestation.ino
@@ -10,9 +10,15 @@
 
 #define CMD_ADDR 0
 #define CMD_SPEED 1
+#define CMD_POINT 2
+
+#define POINT_ON 6
+#define POINT_PA 5
+#define POINT_PB 3
+#define POINT_DELAY 50
 
 typedef struct train_packet {
-  uint8_t cmd;
+  uint16_t cmd;
   uint16_t addr;
   uint8_t data[MAX_DATA_SIZE];
 } Train_Packet;
@@ -31,15 +37,12 @@ typedef struct cv_packet {
   uint8_t cv_data;
 } CV_Packet;
 
-#define FORWARD 1
-#define REVERSE 2
-#define SPEEDINCREMENT 2
-#define STOPLIMIT 20
 
 RF24 radio(4, 7);
 const uint64_t pipes[2] = {0xE7E7E7E7E7, 0xF0F0F0F0D2LL};
 
 DCCCommandStation dcc;
+int point_state = 0;
 
 void setup() {
   Serial.begin(57600);
@@ -56,6 +59,14 @@ void setup() {
   radio.powerUp();
   radio.startListening();
   dcc.setup();
+  pinMode(POINT_ON, OUTPUT);
+  pinMode(POINT_PA, OUTPUT);
+  pinMode(POINT_PB, OUTPUT);
+  delay(1000);
+  point_trigger();
+  delay(1000);
+  point_trigger();
+
 }
 
 Train_Packet tPkt;
@@ -71,12 +82,37 @@ void loop() {
         aPkt = (Addr_Packet *) &(tPkt.data);
         dcc.setAddrShort(tPkt.addr, aPkt->new_addr);
         Serial.println("Set addr");
+        Serial.print(tPkt.addr);
+        Serial.print("-");
+        Serial.println(aPkt->new_addr);
         break;
       case (CMD_SPEED):
         sPkt = (Speed_Packet *) &(tPkt.data);
         dcc.setSpeed128(tPkt.addr, DCC_ADDR_SHORT, sPkt->speed, (DCCDirection) sPkt->dir);
         Serial.println("Set speed");
+        Serial.print(tPkt.addr);
+        Serial.print("-");
+        Serial.println(sPkt->speed);
         break;
+      case (CMD_POINT):
+        if (tPkt.addr == 1){
+          point_trigger();
+          Serial.println("Switching point");
+        }
+        break;
+      default: Serial.println("Not recognised");
     }   
   }
+}
+
+void point_trigger() {
+  if (point_state) digitalWrite(POINT_PA, HIGH);
+  else digitalWrite(POINT_PB, HIGH);
+
+  digitalWrite(POINT_ON, HIGH);
+  delay(POINT_DELAY);
+  digitalWrite(POINT_ON, LOW);
+  digitalWrite(POINT_PA, LOW);
+  digitalWrite(POINT_PB, LOW);
+  point_state = !point_state;
 }
